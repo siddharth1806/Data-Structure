eac68a39a163a08ce1da98484eb3a12a3efd163a
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2020-08-02 01:13:42 +0530

    Create 2DArray.java

---
+++ array/2DArray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class 2DArray{
+    public static void main(String args[])
+    {   
+        Scanner sc=new Scanner(System.in);
+        int sum=0;
+        int arr[][]=new int[6][6];
+        for(int i=0;i<6;i++)
+        {
+            for(int j=0;j<6;j++)
+            {
+                arr[i][j]=sc.nextInt(); 
+            }
+        }
+        int max=-999999;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                if(max==sum)
+                {
+                    max=sum;
+                }
+                else{
+                    if(sum>max)
+                    {
+                        max=sum;
+                    }
+                }                
+            }
+        }
+        System.out.println(max);
+        
+    
+        
+    }
+}
